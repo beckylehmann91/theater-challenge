681c688fef6273d8700a03c195c26b1c59a74a44
beckylehmann91 <beckylehmann91@gmail.com>
2016-03-31 09:53:55 -0500

    Remove customer association from order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  belongs_to :customer, class_name: "User"
+  # belongs_to :customer, class_name: "User"
   belongs_to :showing
 
   validates :name, presence: true
